1c3d2c00010992d7b5952a459db017ddd6012745
Florian <florian.vandaalen@maastro.nl>
2021-06-28 15:55:32 +0200

    Basic setup for a maven java project.
    Contains:
    - checkstyle (basic sun checkstyle)
    - test coverage (jacoco)
    - junit test run

---
+++ src/main/java/Example.java
@@ -0,0 +1,13 @@
+public class Example {
+    public void example() {
+    }
+
+    public void example2() {
+    }
+
+    public void example3() {
+    }
+
+    public void Example4() {
+    }
+}
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void exampletest() {
+        assert (true);
+    }
+
+}
